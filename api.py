c774a30f6d6bfab9bcdb29e7c337afcfa8c7674d
sugitou <sugi_tou108@yahoo.co.jp>
2021-08-30 20:51:52 +0900

    6-3完成

--- api.py
+++ api.py
@@ -2,16 +2,73 @@ import requests
 import urllib
 
 
-def get_api(url):
-    result = requests.get(url)
+def set_param(id, keyword):
+    # 6-2
+    # param = {
+    #     "format" : "json",
+    #     "keyword" : keyword,
+    #     "applicationId" : id,
+    #     "availability" : 0,
+    #     "hits" : 30,
+    #     "page" : 1,
+    #     "sort" : "-updateTimestamp"
+    # }
+
+    # 6-3
+    # param = {
+    #     "format" : "json",
+    #     "keyword" : keyword,
+    #     "applicationId" : id,
+    #     "hits" : 30,
+    #     "page" : 1
+    # }
+    # return param
+
+    # 6-4
+    param = {
+        "format" : "json",
+        "keyword" : keyword,
+        "applicationId" : id,
+        "hits" : 30,
+        "page" : 1
+    }
+    return param
+
+def get_api(url, params):
+    result = requests.get(url, params)
     return result.json()
 
+def output_data(resp):
+    # 6-2
+    # for i, item in enumerate(resp['Items']):
+    #     print('No.', i+1)
+    #     print('itemName :', item['Item']['itemName'])
+    #     print('itemPrice:' + ' ￥' + str(item['Item']['itemPrice']))
+
+    # 6-3
+    # for i, prdct in enumerate(resp['Products']):
+    #     print('No.', i+1)
+    #     print('productName :', prdct['Product']['productName'])
+    #     print('minPrice:' + ' ¥' + str(prdct['Product']['salesMinPrice']))
+    #     print('maxPrice:' + ' ¥' + str(prdct['Product']['salesMaxPrice']))
+
+    # 6-4
+    print()
 
 def main():
     keyword = "鬼滅"
-    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&keyword={}&applicationId=1019079537947262807".format(
-        keyword)
-    print(get_api(url))
+    # 6-2 商品名と価格
+    # url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+    # 6-3 最安値と最高値
+    # url = 'https://app.rakuten.co.jp/services/api/Product/Search/20170426'
+    # 6-4 ランキング
+    url = ''
+    app_id = 1046134668193624354
+
+    params = set_param(app_id, keyword)
+    resp = get_api(url, params)
+
+    output_data(resp)
 
 
 main()
